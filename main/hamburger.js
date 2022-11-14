34f0f3def6b79ae3438531fb00e7f27b092de6d2
somehowjack <bulworm@gmail.com>
2022-11-14 09:13:45 -0500

    Added sticky wrapper for the navbar mene tools and made the animaton got the hamburger work again while still being sticky (or at least it should)

--- main/hamburger.js
+++ main/hamburger.js
@@ -8,14 +8,14 @@ const menuIcon = document.querySelector(".menuIcon");
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
-    menu.style.display = "none";
+    menu.style.visibility = "hidden";
     menu.classList.remove("showMenu");
     blur.style.display = "none";
     // closeIcon.style.display = "none";
     // menuIcon.style.display = "block";
   } else {
     
-    menu.style.display = "inline";
+    menu.style.visibility = "visible";
     menu.classList.add("showMenu");    
     blur.style.display = "inline";
     // closeIcon.style.display = "block";
